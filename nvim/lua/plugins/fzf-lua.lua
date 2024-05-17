fd6086a26ecd79ccd457841714b3a6cddd6787c6
dino <olderfe66@gmail.com>
2024-05-17 23:32:13 +0800

    clear neovim plugins

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -7,15 +7,15 @@ return {
             { "junegunn/fzf", build = "./install --bin" },
         },
         keys = {
-            { "<leader>ff", ":FzfLua files<CR>", desc = "find files" },
-            { "<leader>fg", ":FzfLua live_grep_resume<CR>",  desc = "grep file" },
-            { "<leader>fr", ":FzfLua resume<CR>",     desc = "resume" },
-            { "<leader>fo", ":FzfLua oldfiles<CR>",   desc = "oldfiles" },
-            { "<leader>fb", ":FzfLua buffers<CR>",    desc = "buffers" },
-            { "<leader>fh", ":FzfLua help_tags<CR>",  desc = "help_tags" },
-            { "<leader>/", ":FzfLua lgrep_curbuf<CR>",  desc = "help_tags" },
+            { "<leader>ff", ":FzfLua files<CR>",            desc = "find files" },
+            { "<leader>fg", ":FzfLua live_grep_resume<CR>", desc = "grep file" },
+            { "<leader>fr", ":FzfLua resume<CR>",           desc = "resume" },
+            { "<leader>fo", ":FzfLua oldfiles<CR>",         desc = "oldfiles" },
+            { "<leader>fb", ":FzfLua buffers<CR>",          desc = "buffers" },
+            { "<leader>fh", ":FzfLua help_tags<CR>",        desc = "help_tags" },
+            { "<leader>/",  ":FzfLua lgrep_curbuf<CR>",     desc = "help_tags" },
         },
-         config = function()
+        config = function()
             -- calling `setup` is optional for customization
             require("fzf-lua").setup({})
         end
