a5e4813a35e0f63ababecf3203c65ec5a4d10817
zhujiyuan <jiyuan.zhu@eskyfun.com>
2024-08-11 15:22:45 +0800

    add plugins

--- nvim/lua/plugins/oil-nvim.lua
+++ nvim/lua/plugins/oil-nvim.lua
@@ -1,8 +1,10 @@
 return {
     {
         'stevearc/oil.nvim',
-        opts = {
-            {
+        -- Optional dependencies
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function ()
+            require("oil").setup({
                 -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
                 -- Set to false if you still want to use netrw.
                 default_file_explorer = true,
@@ -52,7 +54,7 @@ return {
                 -- Set to `false` to disable, or "name" to keep it on the file names
                 constrain_cursor = "editable",
                 -- Set to true to watch the filesystem for changes and reload oil
-                experimental_watch_for_changes = false,
+                experimental_watch_for_changes = true,
                 -- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
                 -- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
                 -- Additionally, if it is a string that matches "actions.<name>",
@@ -62,12 +64,15 @@ return {
                 keymaps = {
                     ["g?"] = "actions.show_help",
                     ["<CR>"] = "actions.select",
-                    ["<C-s>"] = "actions.select_vsplit",
-                    ["<C-h>"] = "actions.select_split",
+                    ["<C-v>"] = "actions.select_vsplit",
+                    ["<C-s>"] = "actions.select_split",
                     ["<C-t>"] = "actions.select_tab",
                     ["<C-p>"] = "actions.preview",
                     ["<C-c>"] = "actions.close",
-                    ["<C-l>"] = "actions.refresh",
+                    ["<C-l>"] = false,
+                    ["<C-j>"] = false,
+                    ["<C-k>"] = false,
+                    ["<C-h>"] = false,
                     ["-"] = "actions.parent",
                     ["_"] = "actions.open_cwd",
                     ["`"] = "actions.cd",
@@ -82,7 +87,7 @@ return {
                     border = "rounded",
                 },
                 -- Set to false to disable all of the above keymaps
-                use_default_keymaps = true,
+                use_default_keymaps = false,
                 view_options = {
                     -- Show files and directories that start with "."
                     show_hidden = true,
@@ -176,12 +181,7 @@ return {
                 ssh = {
                     border = "rounded",
                 },
-            }
-        },
-        -- Optional dependencies
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function ()
-            require("oil").setup()
+            })
             vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
         end
     }
