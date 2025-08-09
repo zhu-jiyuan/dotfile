32b196797842564dee6e890f19fba1a754c92038
JiYuan <olderfe66@gmail.com>
2025-08-09 23:23:16 +0800

    feat(install): update create_link shell.

--- create_link.sh
+++ create_link.sh
@@ -1,5 +1,5 @@
 create_symlinks() {
-	src="$PWD/$1"
+	src="$1"
 	target="$2"
 
 	if [ -L "$target" ]; then
@@ -22,24 +22,60 @@ create_symlinks() {
 	echo "[SUCCESS]: $target -> $src"
 }
 
-config_dir="$HOME/.config"
+# 传入一个目录，和指定的目标目录，把该目录下的所有文件和目录创建软链接到目标目录
+create_symlinks_to_target() {
+	src_dir="$1"
+	target_dir="$2"
+	# echo "[DEBUG]: Creating symlinks from $src_dir to $target_dir"
 
-create_symlinks "git/.gitconfig" "$HOME/.gitconfig"
-create_symlinks "yazi" "$config_dir/yazi"
-create_symlinks "tmux" "$config_dir/tmux"
-create_symlinks "rofi" "$config_dir/rofi"
-create_symlinks "zsh" "$config_dir/zsh"
-create_symlinks "zsh/.zshrc" "$HOME/.zshrc"
-create_symlinks "nvim" "$config_dir/nvim"
-create_symlinks "lazygit" "$config_dir/lazygit"
-create_symlinks "kitty" "$config_dir/kitty"
-create_symlinks "i3" "$config_dir/i3"
-create_symlinks "i3status" "$config_dir/i3status"
-create_symlinks "dunst" "$config_dir/dunst"
-create_symlinks ".Xmodmap" "$HOME/.Xmodmap"
-create_symlinks "ghostty" "$config_dir/ghostty"
-create_symlinks "waybar" "$config_dir/waybar"
-create_symlinks "fastfetch" "$config_dir/fastfetch"
-create_symlinks "hypr" "$config_dir/hypr"
-create_symlinks "mako" "$config_dir/mako"
-create_symlinks "fcitx5" "$config_dir/fcitx5"
+	if [ ! -d "$src_dir" ]; then
+		echo "[ERROR]: Source directory $src_dir does not exist."
+		return 1
+	fi
+	if [ ! -d "$target_dir" ]; then
+		echo "[INFO]: Target directory $target_dir does not exist. Creating it."
+		mkdir -p "$target_dir"
+	fi
+	for file in "$src_dir"/*; do
+		if [ -d "$file" ]; then
+			create_symlinks_to_target "$file" "$target_dir/$(basename "$file")"
+		elif [ -f "$file" ]; then
+			create_symlinks "$file" "$target_dir/$(basename "$file")"
+		fi
+	done
+}
+
+config_dir="$HOME/.config"
+# check config directory exists
+if [ ! -d "$config_dir" ]; then
+	echo "[INFO]: $config_dir does not exist. Creating it."
+	mkdir -p "$config_dir"
+fi
+
+local_bin_dir="$HOME/.local/bin"
+if [ ! -d "$local_bin_dir" ]; then
+	echo "[INFO]: $local_bin_dir does not exist. Creating it."
+	mkdir -p "$local_bin_dir"
+fi
+
+create_symlinks "$PWD/git/.gitconfig" "$HOME/.gitconfig"
+create_symlinks "$PWD/yazi" "$config_dir/yazi"
+create_symlinks "$PWD/tmux" "$config_dir/tmux"
+create_symlinks "$PWD/rofi" "$config_dir/rofi"
+create_symlinks "$PWD/zsh" "$config_dir/zsh"
+create_symlinks "$PWD/zsh/.zshrc" "$HOME/.zshrc"
+create_symlinks "$PWD/nvim" "$config_dir/nvim"
+create_symlinks "$PWD/lazygit" "$config_dir/lazygit"
+create_symlinks "$PWD/kitty" "$config_dir/kitty"
+create_symlinks "$PWD/i3" "$config_dir/i3"
+create_symlinks "$PWD/i3status" "$config_dir/i3status"
+create_symlinks "$PWD/dunst" "$config_dir/dunst"
+create_symlinks "$PWD/.Xmodmap" "$HOME/.Xmodmap"
+create_symlinks "$PWD/ghostty" "$config_dir/ghostty"
+create_symlinks "$PWD/waybar" "$config_dir/waybar"
+create_symlinks "$PWD/fastfetch" "$config_dir/fastfetch"
+create_symlinks "$PWD/hypr" "$config_dir/hypr"
+create_symlinks "$PWD/mako" "$config_dir/mako"
+create_symlinks "$PWD/fcitx5" "$config_dir/fcitx5"
+
+create_symlinks_to_target "$PWD/local" "$HOME/.local"
